8727f09c87c2b66588110d8d26bfaf1136500f82
agussalim25 <agussalim5817@guru.smp.belajar.id>
2026-02-02 22:43:57 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Gunakan base empty string atau './' agar path bersifat relatif
   base: './', 
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  server: {
+    port: 3000
   }
 });
